d9c6443b2ae0d3aa4a505e3e8f9daa7be7df28a8
Humphrey Shotton <humpheh@gmail.com>
2014-01-29 13:48:55 +0000

    Basic server API
    Added in a basic server API for accessing JSON data of car parks.

---
+++ server/api.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * API for acessing data via JSON.
+ *
+ * @author	Humphrey Shotton
+ * @version	1.0 (2014-01-28)
+ */
+
+require_once ('init.php');
+
+// Set the content as json
+header('Content-type: application/json; charset=UTF-8');
+
+$stmt = DB::get()->prepare('SELECT *, (SELECT count(*) FROM spaces WHERE space_park_id = park_id) AS ps, 
+			('.get_num_space_query("park_id").') AS spaces FROM parks');
+$stmt->execute();
+$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo '{
+    "carparks":[';
+
+foreach ($res as $row){
+	echo '{
+        "id": '.intval($row['park_id']).',
+        "name": "'.$row['park_name'].'",
+        "desc": "'.$row['park_desc'].'",
+        "totalspaces": '.intval($row['ps']).',
+        "usedspaces": '.intval($row['spaces']).'
+    }';
+	if ($row != end($res)) echo ',';
+}
+echo ']}';
+
+?>
