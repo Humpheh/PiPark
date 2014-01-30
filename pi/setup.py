5ef63cc49cfc50edfd6a76ae53a2abafa2694523
Nick <nicksanders@Nicks-MacBook-Pro.local>
2014-01-30 13:10:45 +0000

    Option 4 to run main.py from setup.py added

--- pi/setup.py
+++ pi/setup.py
@@ -119,8 +119,9 @@ the numbers 1 - 0 to mark new areas.
         
         
         elif user_choice == '4':
+            raw_input("Press ENTER to run the main PiPark program...")
             main.run_main()
-        
+            break
                                                  
         elif user_choice.lower() in ('h', "help"):
             print """
