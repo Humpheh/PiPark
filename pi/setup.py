c38a2069b846ae35f756fa73cc1be4c913e48204
Nick <nicksanders@Nicks-MacBook-Pro.local>
2014-01-30 12:34:28 +0000

    Update file layout and function
    - Code for menu option 1 and 2 neatened and commented.
    - Help and menu option 4 (run main.py) added.
    - User now able to call help and quit by fully typing the words at the
    prompt.

--- pi/setup.py
+++ pi/setup.py
@@ -23,11 +23,11 @@ import setup_selectarea
 # -----------------------------------------------------------------------------
 def __main():
     print """
-    ------------------------------------------
+------------------------------------------
     
-          Setup for CARPARK-O-TRON-9000
+         Setup for PiPark, 2014
     
-    ==========================================
+==========================================
     """
     
     # continually loop menu unitl user chooses 'q' to quit
@@ -36,41 +36,48 @@ def __main():
         user_choice = __menu_choice()
         
         if user_choice == '1':
-            # set setup image save locaiton
-            print "INFO: Setup image save location:", str(setup_image_location)
-            
-            # start picam
-            print "INFO: Camera preview initiated."
+            # initialise camera and start pi camera preview
             camera = imageread.setup_camera()
             camera.start_preview()
             
-            # continually capture images until user accepts.
+            print "INFO: Setup image save location:", str(setup_image_location)
+            print "INFO: Camera preview initiated."
+            print ""
+            
+            # when user presses enter, take a new setup image. Then ask the user to confirm or
+            # reject the image. If image is rejected, take a new image.
             while True:
-                raw_input("Press enter to capture image.")
+                raw_input("When ready, press ENTER to capture setup image.")
                 camera.capture(setup_image_location)
-        
                 user_input = raw_input("Accept image (y/n)? > ")
                 
-                if user_input in ["y", "Y", "yes", "YES"]: break
-            
+                if user_input.lower() in ('y', "yes"): break
+                    
             # picture saved, end preview
-            print "INFO: Setup image has been saved to:", str(setup_image_location)
-            print "      Ending camera preview."
             camera.close()
+            print ""
+            print "INFO: Setup image has been saved to:", str(setup_image_location)
+            print "INFO: Camera preview closed."
         
-           
+        
         elif user_choice == '2':
-            # check setup image exists
+            # check setup image exists, if not print error directing to option 1
             try:
                 setup_image = Image.open(setup_image_location)
-
-                # Run UI Allowing selection of spaces
-                print "\nSelect and mark control-reference locations and parking"
-                print "space locations."
-                setup_selectarea.main(setup_image)
             except:
                 print "ERROR: Setup image does not exist. Select option 1 to"
                 print "create a new setup image."
+            
+            # setup image exists, so open GUI window allowing selection of spaces and
+            # reference areas
+            print """
+            When the window opens use the mouse to mark parking spaces and 3 reference
+            areas. Press T to toggle between marking parking spaces and reference areas and
+            the numbers 1 - 0 to mark new areas.
+            """
+            raw_input("\nPress ENTER to continue...\n")
+            setup_selectarea.main(setup_image)
+        
         
         elif user_choice == '3':
             # attempt to import the setup data and box data
@@ -108,26 +115,42 @@ def __main():
                     senddata.register_area(b[0])
                     print "Registering area", b[0], "on database."
                     
-            print "Registration complete."           
-                                                    
-        elif user_choice in ['q', 'Q']:
+            print "Registration complete."
+        
+        
+        elif user_choice == '4':
+            main.run_main()
+        
+                                                 
+        elif user_choice.lower() in ('h', "help"):
+            print """
+            help message
+            """
+            
+            
+        elif user_choice.lower() in ('q', "quit"):
             print "Setup complete."
             break
         
+        
         else:
             print "\nERROR: Invalid menu choice.\n"
+
+
 
 # -----------------------------------------------------------------------------
 #  Main Menu
 # -----------------------------------------------------------------------------
 def __menu_choice():
     print """
-    At the promt please make a selection.
+At the promt please type a menu selection. For setup instructions type 'help'.
     
-    1 -- Take setup image.
-    2 -- Mark parking spaces and control locations.
-    3 -- Register on server
-    q -- Quit
+1 -- Take setup image.
+2 -- Mark parking spaces and control locations.
+3 -- Register on server
+4 -- Run PiPark main program
+h -- Help
+q -- Quit
     
     """
     
