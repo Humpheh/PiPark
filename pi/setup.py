2cf1398d263a0e93dac4fb723ffb969953567e4d
Nick <nicksanders@Nicks-MacBook-Pro.local>
2014-01-30 13:08:11 +0000

    Help text added to setup.py
    Selecting 'h' or 'help' now displays useful help text.

--- pi/setup.py
+++ pi/setup.py
@@ -71,9 +71,9 @@ def __main():
             # setup image exists, so open GUI window allowing selection of spaces and
             # reference areas
             print """
-            When the window opens use the mouse to mark parking spaces and 3 reference
-            areas. Press T to toggle between marking parking spaces and reference areas and
-            the numbers 1 - 0 to mark new areas.
+When the window opens use the mouse to mark parking spaces and 3 reference
+areas. Press T to toggle between marking parking spaces and reference areas and
+the numbers 1 - 0 to mark new areas.
             """
             raw_input("\nPress ENTER to continue...\n")
             setup_selectarea.main(setup_image)
@@ -124,12 +124,58 @@ def __main():
                                                  
         elif user_choice.lower() in ('h', "help"):
             print """
-            help message
+Welcome to PiPark setup, here's a quick run-through of the steps to successfully
+setup your PiPark unit.
+            
+  1) Ensure that the PiPark unit is mounted suitable above the cark park so that
+     the PiPark's camera is able to look down with a clear view upon the spaces
+     that are required to be monitored.
+            
+  2) At the main menu prompt of this setup program enter option '1' to fine tune
+     the direction of the camera so that it is clearly able to view the spaces.
+     When you have finalised the position of the camera press ENTER to capture a
+     setup image. If everything is correct and you do not wish to recapture the
+     setup image, type 'y' or 'yes' at the prompt to continue the setup program.
+     If you wish to recapture the setup image, type 'n' or 'no' at the prompt.
+     
+     Note: The setup image does not require the car park to be empty.
+
+  3) Once back at the main menu prompt, enter option '2' to open up the setup
+     GUI, which will open a new window allowing you to mark parking space areas
+     and reference areas on the setup image that was captures as part of step 2.
+     
+     When the window has opened use the mouse to mark start and end corners of
+     rectangles, which will represent the areas to be processed by the main
+     program. To toggle between marking reference areas and parking space areas
+     press 'T'. Blue areas represent parking spaces, and red areas represent
+     reference areas.
+     
+     Currently up to ten areas can be marked, using the number keys from 1 - 0.
+     There must be exactly 3 reference areas (RED) marked on the setup image
+     and at least 1 parking space.
+     
+     When all areas have been marked on the image press 'O' to output the marked
+     areas' co-ordinates to a file and close the window to continue completing
+     the setup.
+     
+     Controls Summary:
+       T -- Toggle between reference areas (RED) and parking spaces (BLUE).
+       C -- Clear all marked areas.
+       O -- Output the reference areas to a file.
+       1 to 0 -- Change area ID numbers.
+
+  4) The final step to completing the setup is to choose option '3' from the 
+     main menu prompt. When selected the parking spaces will be registered to
+     the server, and can now be viewed on the website.
+
+  5) The setup is now complete; at the main menu prompt type 'q' or 'quit' to
+     terminate the setup program. Alternatively, choose option 3 to immediately
+     run the main PiPark program.
             """
             
             
         elif user_choice.lower() in ('q', "quit"):
-            print "Setup complete."
+            print ""
             break
         
         
