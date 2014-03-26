c4d12a72b002929b2407a81820ed565c09dd32ef
Humphrey <humpheh@gmail.com>
2014-03-26 21:44:49 +0000

    Fixes to Test Main 2. (Doesn't Capture Images)

--- pi/test_main_2.py
+++ pi/test_main_2.py
@@ -19,17 +19,19 @@ import imageread
 class MainApplication(tk.Frame):
     
     # output messages to the terminal?
-    __is_verbose = False
+    __is_verbose = True
     
     # pi camera
     __camera = None
     __preview_is_active = False
     
-    def __init__(self, master = None):
+    def __init__(self, master = None, camera = None):
         """Application constructor method. """
 
         # run super constructor method
-        tk.Frame.__init__(self, master, camera)
+        tk.Frame.__init__(self, master)
+
+        self.grid()
         
         self.__camera = camera
         
@@ -41,43 +43,47 @@ class MainApplication(tk.Frame):
         self.focus_set()
         
         # run the test app
-        self.run()
+        #self.run()
         
     def escapePressHandler(self, event):
+        if self.__is_verbose: print "ACTION: Escape key pressed."
         
         # if the camera is previewing -> stop the preview
         if self.__camera and self.__preview_is_active:
             self.__camera.stop_preview()
             self.__preview_is_active = False
-            self.set_focus()
+            self.focus_set()
     
     def clickStartPreview(self):
         
         # turn on the camera preview
         if self.__camera and not self.__preview_is_active:
             
-            tkMessagebox.showinfo(
+            tkMessageBox.showinfo(
                 title = "Show Camera Feed",
                 message = "Press the ESCAPE key to exit preview mode"
                 )
             
             self.__camera.start_preview()
             self.__preview_is_active = True
-            self.set_focut()
+            self.focus_set()
     
     def clickQuit(self):
         self.quit()
         self.master.destroy()
     
     def createWidgets(self):
+        if self.__is_verbose: print "INFO: Creating Widgets!"
         
         # create show preview button
-        self.preview_button = tk.Button(title = "Show Camera Feed",
+        self.preview_button = tk.Button(self, text = "Show Camera Feed",
             command = self.clickStartPreview)
+        self.preview_button.grid()
         
         # create quit button
-        self.quit_button = tk.Button(title = "Quit",
+        self.quit_button = tk.Button(self, text = "Quit",
             command = self.clickQuit)
+        self.quit_button.grid()
             
             
     # get/set __is_verbose
@@ -92,9 +98,12 @@ class MainApplication(tk.Frame):
         # >>>>>>>>>>
         # Replace main() code from here!!
         # <<<<<<<<<<
+
+        # image save location
+        image_location = "./images/testimage.jpeg"
         
         while True:
-            camera.capture(image_location)
+            self.__camera.capture(image_location)
             print "INFO: New image captured."
     
             print "INFO: Going to sleep for 5 seconds"
@@ -113,13 +122,12 @@ def main():
     
     """
     
-    # setup camera and image save location
+    # setup camera
     camera = imageread.setup_camera(is_fullscreen = True)
-    image_location = "./images/testimage.jpeg"
     
     # create the application
     root = tk.Tk()
-    app = Application(master = root, camera = camera)
+    app = MainApplication(master = root, camera = camera)
     app.master.title("PiPark 2014")
     app.mainloop()
 
@@ -128,4 +136,11 @@ def main():
 #  Run Program
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
-    main()
+    # setup camera
+    camera = imageread.setup_camera(is_fullscreen = True)
+    
+    # create the application
+    root = tk.Tk()
+    app = MainApplication(master = root, camera = camera)
+    app.master.title("PiPark 2014")
+    app.mainloop()
